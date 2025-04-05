b5ce690a3107927f54a325e20dc8068f13761414
rin-dz <dz.rina.na@gmail.com>
2025-04-06 01:03:06 +0300

    dev: стилизовать NotFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -10,7 +10,9 @@ function NotFound() {
                 <h2 className="not-found__title">404</h2>
                 <p className="not-found__text">Страница не найдена</p>
             </div>
-            <Link className="not-found__link link-button" to="/">Назад</Link>
+            <Link className="not-found__link" to="/">
+                <p4 className="not-found__link-text">Назад</p4>
+            </Link>
         </section>
     )
 }
