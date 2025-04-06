a17b9ae2cc62bdf43c2319054487876a7ff01a1a
rin-dz <dz.rina.na@gmail.com>
2025-04-07 01:21:25 +0300

    dev: создать Animatic

---
+++ src/components/Animatic/Animatic.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Animatic.css";
+
+function Animatic() {
+    return (
+        <div className='animatic'>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+            <li className='animatic__element'></li>
+        </div>
+    )
+}
+
+export default Animatic;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import CurrentUserContext from '../../contexts/CurrentUserContext.js';
 import NotFound from '../NotFound/NotFound';
 import Header from '../Header/Header';
 import Auth from '../Auth/Auth.js';
-
+import Animatic from '../Animatic/Animatic.js'
 
 
 function App() {
   const [currentUser, setCurrentUser] = React.useState({ name: "Пользователь", email: 'example@gmail.com', _id: '14143423' });
-
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
@@ -19,6 +18,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
+        <Animatic />
       </div>
     </CurrentUserContext.Provider>
   );
