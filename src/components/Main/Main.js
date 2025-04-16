d840f4b25e16e46a3fb0ca57d8a7b06297e83be7
rin-dz <dz.rina.na@gmail.com>
2025-04-16 23:31:43 +0300

    dev: первый вариант прокрутки

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -10,8 +10,41 @@ import arrowIcon from '../../images/arrow-left.svg';
 import { movie1, movie2, movie3, movie4, movie5, topMovies } from '../../utils/movies';
 
 function Main(props) {
+    const [slidesPosition, changePosition] = React.useState(0);
+    const [rightButtonVisibility, changeRightButtonVisibility] = React.useState(true);
+    const [leftButtonVisibility, changeLeftButtonVisibility] = React.useState(false);
 
     const movies = [movie2, movie1, movie4, movie5, movie2, movie4, movie3, movie1, movie3, movie5];
+
+    // сохздать переменную с постоянной величиной прокрутки для корусельки вместо 559, которая будет меняться при событии
+    // resize у экрана !!добавить eventListener 'resize' у окошка - нр от 900 px до 400px прокрутка будет равна 300px и тд
+
+    function leftScroll() {
+        if (slidesPosition < 0) {
+            changeRightButtonVisibility(true);
+            if (slidesPosition + 559 > 0) {
+                changePosition(0);
+                changeLeftButtonVisibility(false);
+            } else {
+                changePosition(slidesPosition + 559);
+                slidesPosition + 559 === 0 ? changeLeftButtonVisibility(false) : changeLeftButtonVisibility(true);
+            }
+        }
+    }
+
+
+    function rightScroll() {
+        if (slidesPosition > -2885) {
+            changeLeftButtonVisibility(true);
+            if (slidesPosition - 559 < -2885 + window.innerWidth) {
+                changePosition(-2885 + window.innerWidth);
+                changeRightButtonVisibility(false);
+            } else {
+                changePosition(slidesPosition - 559);
+                slidesPosition - 570 < -2885 + window.innerWidth ? changeRightButtonVisibility(false) : changeRightButtonVisibility(true);
+            }
+        }
+    }
 
     //для прокрутки карусельки можно сделать отрисовку первых пяти элементов массива, где будут объекты 
     // с информацией о слайдах и при нажатии на левую стрелку будет удаляться объект с конца и он же
@@ -22,8 +55,6 @@ function Main(props) {
     // так же можно будет прятать кнопки прокрутки, если коруселько уже в максимально правом или максимально левом положении
     // мб нажатие будет только одно, чтобы разом прокрутить до конца
 
-    // owerflow-y: hidden !!!
-
     // Приходит по 10 фильмов, далее нужно отправлять такой же запрос с другой страницей
 
     return (
@@ -33,15 +64,21 @@ function Main(props) {
                 <div className='main__slides'>
                     <h2 className='main__slide-title'>Топ-10 на IMDb на этой неделе:</h2>
                     <div className='main__slide-container'>
-                        <button className='main__button-container main__left-button'>
-                            <img className='main__slides-button' src={arrowIcon} alt='Налево' />
-                        </button>
-                        {topMovies.map((el) => (
-                            <Slide movie={el} isTopSlide={true} />
-                        ))}
-                        <button className='main__button-container main__right-button'>
-                            <img className='main__slides-button' src={arrowIcon} alt='Направо' />
-                        </button>
+                        {leftButtonVisibility ?
+                            <button className='main__button-container main__left-button' onClick={leftScroll}>
+                                <img className='main__slides-button' src={arrowIcon} alt='Налево' />
+                            </button> : ''
+                        }
+                        <div className='main__slides-scrolling' id='target' style={{ left: `${slidesPosition}px`, }}>
+                            {topMovies.map((el) => (
+                                <Slide movie={el} isTopSlide={true} />
+                            ))}
+                        </div>
+                        {rightButtonVisibility ?
+                            <button className='main__button-container main__right-button'>
+                                <img className='main__slides-button' src={arrowIcon} alt='Направо' onClick={rightScroll} />
+                            </button> : ''
+                        }
                     </div>
                 </div>
                 <SearchTab />
